3af8895a654b8eded9a818aa50a170ebcf3f386e
Matheus Borges <matheusvbborges@hotmail.com>
2024-09-05 20:42:09 -0300

    implementação da tradução da sinopse

--- src/main/java/br/com/alura/screenmatchWeb/main/Main.java
+++ src/main/java/br/com/alura/screenmatchWeb/main/Main.java
@@ -45,7 +45,7 @@ public class Main {
                     listarSeriesBuscadas();
                     break;
                 case 0:
-                    System.out.println("Saindo...");
+                    System.out.println("Saindo...".);
                     break;
                 default:
                     System.out.println("Opção inválida");
