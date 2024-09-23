266b8febbb11f340764bf6e244ff55cbe4b134b8
Matheus Borges <matheusvbborges@hotmail.com>
2024-09-23 15:22:46 -0300

    adicionando mais opções de busca

--- src/main/java/br/com/alura/screenmatchWeb/main/Main.java
+++ src/main/java/br/com/alura/screenmatchWeb/main/Main.java
@@ -35,6 +35,11 @@ public class Main {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas
+                    4 - Buscar série por titulo
+                    5 - Buscar série por ator
+                    6 - Buscar as top 5 séries
+                    7 - Buscar séries por categoria
+                    8 - Buscar séries por total de temporada
 
                     0 - Sair                                 
                     """;
@@ -53,8 +58,22 @@ public class Main {
                 case 3:
                     listarSeriesBuscadas();
                     break;
+                case 4:
+                    buscarSeriePorTitulo();
                 case 0:
                     System.out.println("Saindo...");
+                    break;
+                case 5:
+                    buscarSeriePorAtor();
+                    break;
+                case 6:
+                    buscarTop5Series();
+                    break;
+                case 7:
+                    buscarSeriePorCategoria();
+                    break;
+                case 8:
+                    buscarSeriePorTotalTemporada();
                     break;
                 default:
                     System.out.println("Opção inválida");
@@ -62,7 +81,85 @@ public class Main {
         }
     }
 
-        private void buscarSerieWeb() {
+    private void buscarSeriePorTotalTemporada() {
+        System.out.println("Digite o numero minimo de temporadas que deseja para a busca: ");
+        var totalTemporada = scanner.nextInt();
+
+        if (totalTemporada < 0) {
+            System.out.println("O número de temporadas deve ser positivo.");
+            return;
+        }
+
+        scanner.nextLine();
+        System.out.println("Digite a avaliação minima: ");
+        var entradaAvaliacao = scanner.nextLine().replace('.', ',');
+
+        double avaliacaoMinima;
+
+        try {
+            avaliacaoMinima = Double.parseDouble(entradaAvaliacao.replace(',', '.'));
+        } catch (NumberFormatException e) {
+            System.out.println("Avaliação inválida! Por favor, insira um número.");
+            return;
+        }
+        List<Serie> seriePorTotalTemporada = repositorio.findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(totalTemporada, avaliacaoMinima);
+        if (seriePorTotalTemporada.isEmpty()) {
+            System.out.println("Nenhuma série encontrada!");
+        } else {
+            System.out.println("Séries encontradas: ");
+            seriePorTotalTemporada.forEach(s -> System.out.println(s.getTitulo()));
+        }
+
+
+    }
+
+    private void buscarSeriePorCategoria() {
+        System.out.println("Deseja buscar séries de qual categoria? ");
+        var nomeGenero = scanner.nextLine();
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        List<Serie> seriesCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Séries da categoria " + nomeGenero);
+        seriesCategoria.forEach(System.out::println);
+
+    }
+
+    private void buscarTop5Series() {
+        List<Serie> seriesTop5 = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        seriesTop5.forEach(s -> System.out.println(s.getTitulo() + " / avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarSeriePorAtor() {
+        System.out.println("Qual o nome para busca? ");
+        var nomeAtor = scanner.nextLine();
+        System.out.println("Avalições a partir de qual valor? ");
+        var entradaAvaliacao = scanner.nextLine().replace('.', ',');
+
+        double avaliacao;
+
+        try {
+            avaliacao = Double.parseDouble(entradaAvaliacao.replace(',', '.'));
+        } catch (NumberFormatException e) {
+            System.out.println("Avaliação inválida! Por favor, insira um número.");
+            return;
+        }
+
+        List<Serie> seriesEncontrada = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor,avaliacao);
+        System.out.println("Séries em que o " + nomeAtor + " trabalhou");
+        seriesEncontrada.forEach(s -> System.out.println(s.getTitulo() + " / avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma série pelo nome: ");
+        var nomeSerie = scanner.nextLine();
+        Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+        if (serieBuscada.isPresent()) {
+            System.out.println("Dados da série: " + serieBuscada.get());
+        } else{
+            System.out.println("Série não encontrada");
+        }
+    }
+
+    private void buscarSerieWeb() {
             DadosSerie dados = getDadosSerie();
             Serie serie = new Serie(dados);
             //dadosSerie.add(dados);
@@ -82,10 +179,7 @@ public class Main {
         System.out.println("Escolha uma série pelo nome: ");
         var nomeSerie = scanner.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
-
+        Optional<Serie> serie = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
         if (serie.isPresent()) {
 
             var serieEncontrada = serie.get();
--- src/main/java/br/com/alura/screenmatchWeb/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatchWeb/repository/SerieRepository.java
@@ -1,8 +1,21 @@
 package br.com.alura.screenmatchWeb.repository;
 
+import br.com.alura.screenmatchWeb.model.Categoria;
 import br.com.alura.screenmatchWeb.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
 
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor,Double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(Integer totalTemporadas, Double avaliacao);
 }
